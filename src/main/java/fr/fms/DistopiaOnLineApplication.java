a380229f0a4dc371f97d7ac95702bb1182f5cfd8
Delmerie <j.delmerie@live.fr>
2022-07-05 10:12:44 +0200

    first commit

---
+++ src/main/java/fr/fms/DistopiaOnLineApplication.java
@@ -0,0 +1,13 @@
+package fr.fms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistopiaOnLineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DistopiaOnLineApplication.class, args);
+	}
+
+}
